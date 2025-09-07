0e328dcb5ab3bc87b90f554af2be16827c78c0e7
Sascha Hinlang <sascha.hinlang@thelighthousepublishing.com>
2025-09-07 11:31:59 +0200

    Enabled snacks dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,7 +8,7 @@ return {
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
 		bigfile = { enabled = false },
-		dashboard = { enabled = false },
+		dashboard = { enabled = true },
 		explorer = { enabled = true },
 		indent = { enabled = false },
 		input = { enabled = false },
