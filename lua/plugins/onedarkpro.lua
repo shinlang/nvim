f63ba05f61661cf9b231e8ad28f4d9612ec5ab12
Sascha Hinlang <sascha.hinlang@thelighthousepublishing.com>
2025-09-08 09:38:51 +0200

    Fixed theme colors of gitsigns

--- lua/plugins/onedarkpro.lua
+++ lua/plugins/onedarkpro.lua
@@ -2,7 +2,27 @@ return {
 	"olimorris/onedarkpro.nvim",
 	priority = 1000, -- Ensure it loads first
 	config = function()
-		require("onedarkpro").setup()
+		local color = require("onedarkpro.helpers")
+		local colors = color.get_colors()
+
+		require("onedarkpro").setup({
+			highlights = {
+				GitSignsAdd = { fg = colors.green },
+				GitSignsAddCursorLine = { fg = colors.green },
+				GitSignsChange = { fg = colors.yellow },
+				GitSignsChangeCursorLine = { fg = colors.yellow },
+				GitSignsDelete = { fg = colors.red },
+				GitSignsDeleteCursorLine = { fg = colors.red },
+				GitSignsUntracked = { fg = colors.blue },
+				GitSignsUntrackedCursorLine = { fg = colors.blue },
+
+				GitSignsAddPreview = { bg = colors.green },
+				GitSignsDeletePreview = { bg = colors.red },
+				GitSignsAddInline = { bg = colors.green },
+				GitSignsChangeInline = { bg = colors.yellow },
+				GitSignsDeleteInline = { bg = colors.red },
+			},
+		})
 		vim.cmd("colorscheme onedark")
 	end,
 }
