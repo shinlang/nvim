fbf5824ecc36383309cb7b23df14037c36362b65
Sascha Hinlang <sascha.hinlang@phoenixgames.com>
2025-09-16 14:58:29 +0200

    Added mini.comment

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,4 +2,5 @@ return {
 	{ "nvim-mini/mini.pairs", version = "*", opts = {} },
 	{ "nvim-mini/mini.move", version = "*", opts = {} },
 	{ "nvim-mini/mini.icons", version = "*", opts = {} },
+	{ "nvim-mini/mini.comment", version = "*", opts = {} },
 }
