5a967744fdd6afbd77519c231981c496bc94140f
Sascha Hinlang <sascha.hinlang@phoenixgames.com>
2026-01-19 21:36:56 +0100

    Added keymap for diffing

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -29,3 +29,39 @@ vim.keymap.set('t', '<A-h>', '<C-\\><C-n><C-w>h', { noremap = true, silent = tru
 vim.keymap.set('t', '<A-j>', '<C-\\><C-n><C-w>j', { noremap = true, silent = true })
 vim.keymap.set('t', '<A-k>', '<C-\\><C-n><C-w>k', { noremap = true, silent = true })
 vim.keymap.set('t', '<A-l>', '<C-\\><C-n><C-w>l', { noremap = true, silent = true })
+
+-- Diff
+vim.keymap.set('n', '<leader>dd', function()
+  local bufs = vim.fn.getbufinfo({ buflisted = 1 })
+  if #bufs < 2 then
+    print("Need at least 2 listed buffers")
+    return
+  end
+
+  local curr = vim.api.nvim_get_current_buf()
+
+  -- find current buffer in listed order
+  local next_buf
+  for i, info in ipairs(bufs) do
+    if info.bufnr == curr then
+      -- wrap around to first if at end
+      next_buf = bufs[i + 1] and bufs[i + 1].bufnr or bufs[1].bufnr
+      break
+    end
+  end
+
+  if not next_buf then
+    print("Could not find next buffer")
+    return
+  end
+
+  -- diff current with next buffer
+  -- open vertical split with next buffer
+  vim.cmd("vsplit")
+  vim.api.nvim_set_current_buf(next_buf)
+  vim.cmd("diffthis")
+
+  -- go back to original and diff
+  vim.api.nvim_set_current_win(vim.fn.win_getid(vim.fn.winnr('#')))
+  vim.cmd("diffthis")
+end, { desc = "Diff current buffer with next buffer" })
