5b69b1f4ec5fec73bfade0554ec2001243d32808
Sascha Hinlang <sascha.hinlang@phoenixgames.com>
2026-01-20 10:56:53 +0100

    Use spaces instead of tabs for indentation

--- init.lua
+++ init.lua
@@ -24,6 +24,11 @@ vim.o.cursorline = true
 vim.o.scrolloff = 10
 vim.o.confirm = true
 
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
 require("config.keymaps")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
